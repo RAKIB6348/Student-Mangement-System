a4f3a76aba74eaf44eaf89483fdccf168c023a29
RAKIB6348 <shahidulhuda0@gmail.com>
2025-12-08 22:58:42 +0600

    create add teacher page and urls

--- teacher/urls.py
+++ teacher/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('teacher-list', teacher_list, name='teacher_list'),
+    path('teacher-create/', add_teacher, name='add_teacher'),
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def teacher_list(request):
 
     return HttpResponse('This is a teacher list page')
+
+
+
+def add_teacher(request):
+
+    return render(request, 'Teacher/add_teacher.html')
