4571093480340a2604f525177f52ec42d9eb8338
RAKIB6348 <shahidulhuda0@gmail.com>
2025-12-09 15:04:10 +0600

    create log out url and set login_required

--- student/views.py
+++ student/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def student_list(request):
@@ -13,7 +14,7 @@ def student_create(request):
     return render(request, 'Student/add_student.html')
 
 
-
+@login_required
 def student_dashboard(request):
 
     return render(request, 'Student/student_dashboard.html')
--- teacher/views.py
+++ teacher/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def teacher_list(request):
@@ -13,7 +14,7 @@ def add_teacher(request):
     return render(request, 'Teacher/add_teacher.html')
 
 
-
+@login_required
 def teacher_dashboard(request):
     
     return render(request, "Teacher/teacher_dashboard.html")
